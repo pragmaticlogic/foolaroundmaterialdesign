19633364dc19403d75bd79df1367b208d60ad44b
Kevin <kevinhle@outlook.com>
2015-09-20 18:01:39 -0500

    got login page look ok

--- app/src/main/java/com/codeprototype/kevin/foolaroundmaterialdesign/activity/LoginActivity.java
+++ app/src/main/java/com/codeprototype/kevin/foolaroundmaterialdesign/activity/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.codeprototype.kevin.foolaroundmaterialdesign.activity;
 
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -13,27 +14,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_login, menu);
-        return true;
-    }
+        final TextInputLayout usernameWrapper = (TextInputLayout) findViewById(R.id.usernameWrapper);
+        final TextInputLayout passwordWrapper = (TextInputLayout) findViewById(R.id.passwordWrapper);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        usernameWrapper.setHint("Username");
+        passwordWrapper.setHint("Password");
     }
 }
