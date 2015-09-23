8bcc5335ae17512f1e1b4186a5a6627eeb3a4c82
Kevin Le <pragmaticlogic@gmail.com>
2015-09-22 23:22:01 -0500

    add toolbar to login page

--- app/src/main/java/com/codeprototype/kevin/foolaroundmaterialdesign/activity/LoginActivity.java
+++ app/src/main/java/com/codeprototype/kevin/foolaroundmaterialdesign/activity/LoginActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.support.v7.widget.Toolbar;
 
 import com.codeprototype.kevin.foolaroundmaterialdesign.R;
 
@@ -20,5 +21,13 @@ public class LoginActivity extends AppCompatActivity {
 
         usernameWrapper.setHint("Username");
         passwordWrapper.setHint("Password");
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        if(toolbar != null) {
+            setSupportActionBar(toolbar);
+            getSupportActionBar().setTitle(R.string.welcome_label);
+            getSupportActionBar().setHomeButtonEnabled(true);
+            //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
     }
 }
