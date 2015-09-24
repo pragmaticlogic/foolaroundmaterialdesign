7fa348b81f276dfb3ecd7ca311502097f338a215
Kevin <kevinhle@outlook.com>
2015-09-23 22:53:01 -0500

    added signup

--- app/src/main/java/com/codeprototype/kevin/foolaroundmaterialdesign/activity/LoginActivity.java
+++ app/src/main/java/com/codeprototype/kevin/foolaroundmaterialdesign/activity/LoginActivity.java
@@ -1,14 +1,19 @@
 package com.codeprototype.kevin.foolaroundmaterialdesign.activity;
 
+import android.content.Intent;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.codeprototype.kevin.foolaroundmaterialdesign.R;
 
 public class LoginActivity extends AppCompatActivity {
+
+    protected Button _signupTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +23,15 @@ public class LoginActivity extends AppCompatActivity {
         final TextInputLayout usernameWrapper = (TextInputLayout) findViewById(R.id.usernameWrapper);
         final TextInputLayout passwordWrapper = (TextInputLayout) findViewById(R.id.passwordWrapper);
 
-        usernameWrapper.setHint("Username");
-        passwordWrapper.setHint("Password");
+        _signupTextView = (Button) findViewById(R.id.signupButton);
+        _signupTextView.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+        //usernameWrapper.setHint("Username");
+        //passwordWrapper.setHint("Password");
     }
 }
